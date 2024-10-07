31342d1fdaa241d731c5c57c2e70da9c7f732a52
waleedsh02 <waleedshahid02@gmail.com>
2024-10-06 22:01:17 -0400

    First commit

---
+++ src/main/java/com/example/lab5tipcalculator/TipCalculatorController.java
@@ -0,0 +1,92 @@
+package com.example.lab5tipcalculator;
+
+// TipCalculatorController.java
+// Controller that handles events using property listeners
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+public class TipCalculatorController {
+    // formatters for currency and percentages
+    private static final NumberFormat currency =
+            NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percent =
+            NumberFormat.getPercentInstance();
+
+    private BigDecimal tipPercentage = new BigDecimal(0.15); // 15% default
+
+    // GUI controls defined in FXML and used by the controller's code
+    @FXML
+    private TextField amountTextField;
+
+    @FXML
+    private Label tipPercentageLabel;
+
+    @FXML
+    private Slider tipPercentageSlider;
+
+    @FXML
+    private TextField tipTextField;
+
+    @FXML
+    private TextField totalTextField;
+
+    // this method calculates and displays the total amount due with the tip
+    private void calculate() {
+        try {
+            BigDecimal amount = new BigDecimal(amountTextField.getText());
+            BigDecimal tip = amount.multiply(tipPercentage);
+            BigDecimal total = amount.add(tip);
+
+            tipTextField.setText(currency.format(tip));
+            totalTextField.setText(currency.format(total));
+        }
+        catch (NumberFormatException ex) {
+            amountTextField.setText("Enter amount");
+            amountTextField.selectAll();
+            amountTextField.requestFocus();
+        }
+    }
+
+    // called by FXMLLoader to initialize the controller
+    public void initialize() {
+        // 0-4 rounds down, 5-9 rounds up
+        currency.setRoundingMode(RoundingMode.HALF_UP);
+
+        // listener for changes to tipPercentageSlider's value
+        tipPercentageSlider.valueProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> ov,
+                                        Number oldValue, Number newValue) {
+                        tipPercentage =
+                                BigDecimal.valueOf(newValue.intValue() / 100.0);
+                        tipPercentageLabel.setText(percent.format(tipPercentage));
+                        calculate(); // this helps us to recalculate whenever the slider changes
+                    }
+                }
+        );
+
+        // listener added for changes to amountTextField's text
+        amountTextField.textProperty().addListener(
+                new ChangeListener<String>() {
+                    @Override
+                    public void changed(ObservableValue<? extends String> ov,
+                                        String oldValue, String newValue) {
+                        calculate(); // recalculate whenever amount changes
+                    }
+                }
+        );
+
+        // Binded  the tipPercentageLabel to reflect the current value of the slider
+        tipPercentageLabel.textProperty().bind(
+                tipPercentageSlider.valueProperty().asString("%.0f%%")
+        );
+    }
+}
